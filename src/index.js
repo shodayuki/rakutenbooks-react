e42a5543f0d4485fa6d99b589b06febffdff5b27
shodayuki <yshoda1227@gmail.com>
2020-11-07 10:02:58 +0900

    Hello World

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <HelloWorldChild />
+      </div>
+    );
+  }
+}
+
+const HelloWorldChild = () => {
+  return (
+    <h1 class="l-headerTitle"> Hello World</h1>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App />,
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
